2f8f0b5be173d6fe30804d93eca12d95ecd834f2
unknown <lucie.g@codeur.online>
2017-12-21 16:37:26 +0100

    changements de structure antoine et lucie

---
+++ index.php
@@ -0,0 +1,151 @@
+<?php
+
+require "./vendor/autoload.php";
+
+
+
+$router = new AltoRouter();
+
+
+$router->setBasePath('data_musees/');
+
+
+
+$loader = new Twig_Loader_Filesystem('./views');
+
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+));
+
+
+
+$router->map( 'GET', '/', function() {
+    
+    
+    global $twig;
+    
+    
+    $template = $twig->load('basic.html.twig');
+    $data = ['Lucie', 'Floriane', 'Magalie', 'Antoine', 'Mourad'];
+    
+    $params =["tab
+    " => $data];
+    
+    
+    
+    
+    
+    echo $template->render($params);
+   
+    
+    
+    
+});
+
+
+$router->map( 'GET', '/test', function() {
+   
+    echo "vous etes sur test !!";
+    
+    
+    
+});
+
+
+
+$router->map( 'GET', '/home', function() {
+   
+     global $twig;
+     $template = $twig->load('home.html.twig');
+    
+     echo $template->render();
+    
+});
+
+
+
+$router->map( 'GET', '/region/[a:region]', function($region) {
+   
+        include_once './services/Utils.php';
+      
+        $new_region =  Utils::parseRegion($region);
+        
+    
+        if($new_region == false){
+            
+            header("Location: http://localhost/data_musees/home");
+            
+        }else{
+            
+              include_once './models/dbconfig.php';
+              include_once './models/musees.php';
+            
+              $data = getMuseumByLand($pdo, $new_region);
+              global $twig;
+              $template = $twig->load('list-regions.html.twig');
+    
+              echo $template->render([
+                  'musees' => $data, 
+                  'region' => $new_region
+              ]);
+                
+            
+        }
+    
+    
+        
+   
+    
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( $match && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+
+
+
+?>
---
+++ services/Utils.php
@@ -0,0 +1,97 @@
+<?php
+class Utils{
+    
+    static function parseRegion($current_region){
+        switch ($current_region) {
+                case "mayotte":
+                    $region = "MAYOTTE";
+                    break;
+
+                case "grandEst":
+                   $region = "GRAND-EST";
+                    break;
+
+                case "Normandie":
+                    $region = "NORMANDIE";
+                    break;
+
+                case "nouvelleAquitaine":
+                    $region = "NOUVELLE-AQUITAINE";
+                    break;
+
+                case "occitanie":
+                    $region = "OCCITANIE";
+                    break;
+
+                case "auvergneRhoneAlpes":
+                    $region = "AUVERGNE-RHÔNES-ALPES";
+                    break;
+
+                case "bourgogneFrancheComte":
+                    $region = "BOURGOGNE-FRANCHE-COMTE";
+                    break;
+
+                case "bretagne":
+                    $region = "BRETAGNE";
+                    break;
+
+                case "centreValLoire":
+                    $region = "CENTRE-VAL-DE-LOIRE";
+                    break;
+
+                case "corse":
+                    $region = "CORSE";
+                    break;
+
+                case "Guadeloupe":
+                    $region = "GUADELOUPE";
+                    break;
+
+                case "Guadeloupe":
+                    $region = "GUADELOUPE";
+                    break;
+
+                case "ileDeFrance":
+                    $region = "ILE-DE-FRANCE";
+                    break;
+
+                case "hautDeFrance":
+                    $region = "HAUTS-DE-FRANCE";
+                    break;
+
+                 case "paysLoire":
+                    $region = "PAYS-DE-LA-LOIRE";
+                    break;
+
+                  case "guyane":
+                    $region = "GUYANE";
+                    break;
+
+                  case "paca":
+                    $region = "PROVENCE-ALPES-COTESD'AZUR";
+                    break;  
+
+                   case "reunion":
+                    $region = "REUNION";
+                    break; 
+
+                  case "martinique":
+                    $region = "MARTINIQUE";
+                    break;
+                  default:
+                        $region = false;
+            }
+        
+        return $region;
+        
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+?>
