96f8b26815e878bb886280ef867162d10452e0ec
Lefromager <lefromager.gaspard15@gmail.com>
2017-12-22 15:58:01 +0100

    super requete

--- models/musees.php
+++ models/musees.php
@@ -29,11 +29,9 @@ function getRegion($pdo, $region){
 }
 
 
-
-
 function getDepartements($pdo){
 
-	$stmt = $pdo->prepare("SELECT DISTINCT nom_dep FROM musee ");
+	$stmt = $pdo->prepare("SELECT DISTINCT nom_dep  FROM musee ");
     
     $stmt->execute();
     
@@ -41,7 +39,49 @@ function getDepartements($pdo){
     
     return  $result;
 
+}
+
+function getAllReg($pdo){
+
+	$stmt = $pdo->prepare("SELECT DISTINCT nom_reg  FROM musee ");
+    
+    $stmt->execute();
+    
+    $result = $stmt->fetchAll();
+    
+    return  $result;
 
 }
 
+
+
+
+
+function getGroup($pdo){
+
+	$stmt = $pdo->prepare("SELECT nom_reg, fonction nom_dep FROM musee");
+    
+    $stmt->execute();
+    
+    $result = $stmt->fetchAll();
+    
+    return  $result;
+
+}
+
+function getDepByReg($pdo, $region){
+
+	$stmt = $pdo->prepare("SELECT DISTINCT nom_dep FROM musee WHERE nom_reg = :region");
+    $stmt->bindParam(':region', $region);
+    
+    $stmt->execute();
+    
+    $result = $stmt->fetchAll();
+    
+    return  $result;
+
+}
+
+
 ?>
+
